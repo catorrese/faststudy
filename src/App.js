67bc77de2a951d3b7464754bd84dba2f9d214b96
Carlos Torres <carlos.torres@trademate.tech>
2022-11-02 22:25:38 -0500

    Login structure implemented

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Wrapper from './components/wrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Wrapper></Wrapper>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
